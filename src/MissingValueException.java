ccd00e5c5e672c8865eb606ecdf6f75215c31dd3
Afaren <550047450@qq.com>
2017-03-24 13:03:06 +0800

    afaren - clean up project structure

--- src/MissingValueException.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by afaren on 8/30/16.
- */
-public class MissingValueException extends RuntimeException{
-
-    public MissingValueException(String message) {
-        super(message);
-    }
-}
